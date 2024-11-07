4c60e9be0a528daf58b501c1786be9751e98c275
heartuser <546239212@qq.com>
2024-11-07 09:38:09 +0800

    矩阵按键增加注释

--- 矩阵按键/src/main.c
+++ 矩阵按键/src/main.c
@@ -1,18 +1,22 @@
 #include "Int_matrixSwitch.h"
 #include "Int_DigitalTube.h"
 
+/*
+    按下按键，显示对应的按键编号
+*/
+
 void main()
 {
     u8 switchNum;
     Int_DigitalTube_Init();
     while (1)
     {
-        switchNum = witchSwitchPressed();
+        switchNum = witchSwitchPressed();// 获取被按下按键的编号，如果没有按键被按下，则得到0
         //在数码管上显示数字
-        if (switchNum){
+        if (switchNum){// 如果有按键被按下，改变要显示的数字
             Int_DigitalTube_DisplayNum(switchNum);
         }
-        Int_DigitalTube_Refresh();
+        Int_DigitalTube_Refresh();// 显示数字
     }
     
 }
