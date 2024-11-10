c571bb7e3b782db60b9b3b610d68c908539a6c93
heartuser <546239212@qq.com>
2024-11-10 13:09:44 +0800

    定时器

---
+++ 定时器/src/main.c
@@ -0,0 +1,19 @@
+#include "Dri_Timer0.h"
+#include <STC89C5xRC.H>
+
+void led_Blink()
+{
+    static int i = 0;
+    if (i == 200) {
+        P20 = ~P20;
+        i   = 0;
+    }
+    i++;
+}
+
+void main()
+{
+    Dri_Timer0_Init();
+    Dri_Timer0_RegisterCallBack(&led_Blink);
+    while (1);
+}
